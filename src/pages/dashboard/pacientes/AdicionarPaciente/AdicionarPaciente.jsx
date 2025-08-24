b49226479776e535786905564b35412cb7a37625
GuiGavassa <guilhermegavassa.tecn@gmail.com>
2025-08-24 20:51:08 -0300

    fix alteração no tamanho do card cadastrar paciente

--- src/pages/dashboard/pacientes/AdicionarPaciente/AdicionarPaciente.jsx
+++ src/pages/dashboard/pacientes/AdicionarPaciente/AdicionarPaciente.jsx
@@ -88,63 +88,67 @@ const AdicionarPaciente = () => {
                         <h2>Dados Do Paciente:</h2>
                         <section>
 
-                            <InputField
-                                labelTitle={'Nome'}
-                                value={nome}
-                                width={'w-full'}
-                                containerWidth={'w-[80%]'}
-                                placeholder={'Nome do paciente'}
-                                onChange={(e) => setNome(e.target.value)}
-                                required
-                            />
-                            <InputField
-                                labelTitle={'E-mail'}
-                                type={'email'}
-                                value={email}
-                                width={'w-full'}
-                                containerWidth={'w-[80%]'}
-                                placeholder={'E-mail do paciente'}
-                                onChange={(e) => setEmail(e.target.value)}
-                                required
-                            />
-
-                            <div className="select-container">
-                                <label htmlFor="diaConsultas">Dia de Consultas</label>
-                                <select
-                                    id="diaConsultas"
-                                    value={diaConsultas}
-                                    className='select-field w-full'
-                                    onChange={(e) => setDiaConsultas(e.target.value)}
+                            <div className='w-full justify-center flex-col items-center flex gap-4 mb-4'>
+                                <InputField
+                                    labelTitle={'Nome'}
+                                    value={nome}
+                                    width={'w-full'}
+                                    containerWidth={'w-[80%]'}
+                                    placeholder={'Nome do paciente'}
+                                    onChange={(e) => setNome(e.target.value)}
                                     required
-                                >
-                                    <option value="" disabled>Selecione um dia</option>
-                                    <option value="SEGUNDA">Segunda-feira</option>
-                                    <option value="TERCA">Terça-feira</option>
-                                    <option value="QUARTA">Quarta-feira</option>
-                                    <option value="QUINTA">Quinta-feira</option>
-                                    <option value="SEXTA">Sexta-feira</option>
-                                </select>
+                                />
+                                <InputField
+                                    labelTitle={'E-mail'}
+                                    type={'email'}
+                                    value={email}
+                                    width={'w-full'}
+                                    containerWidth={'w-[80%]'}
+                                    placeholder={'E-mail do paciente'}
+                                    onChange={(e) => setEmail(e.target.value)}
+                                    required
+                                />
                             </div>
-                            <div className="select-container">
-                                <label htmlFor="horarioConsultas">Horário de Consultas</label>
-                                <select
-                                    id="horarioConsultas"
-                                    value={horarioConsultas}
-                                    className='select-field w-full'
-                                    onChange={(e) => setHorarioConsultas(e.target.value)}
-                                    required
-                                >
-                                    <option value="" disabled>Selecione um horário</option>
-                                    {Array.from({ length: 9 }, (_, i) => {
-                                        const hour = (8 + i).toString().padStart(2, '0');
-                                        if (hour === "12") return null;
-                                        return (
-                                            <option key={hour} value={`${hour}:00`}>
-                                                {`${hour}:00`}
-                                            </option>
-                                        );
-                                    })}
-                                </select>
+                            <div className='w-full justify-center flex-col items-center flex gap-4 mb-4'>
+                                <div className="select-container">
+                                    <label htmlFor="diaConsultas">Dia de Consultas</label>
+                                    <select
+                                        id="diaConsultas"
+                                        value={diaConsultas}
+                                        className='select-field w-full'
+                                        onChange={(e) => setDiaConsultas(e.target.value)}
+                                        required
+                                    >
+                                        <option value="" disabled>Selecione um dia</option>
+                                        <option value="SEGUNDA">Segunda-feira</option>
+                                        <option value="TERCA">Terça-feira</option>
+                                        <option value="QUARTA">Quarta-feira</option>
+                                        <option value="QUINTA">Quinta-feira</option>
+                                        <option value="SEXTA">Sexta-feira</option>
+                                    </select>
+                                </div>
+                                <div className="select-container">
+                                    <label htmlFor="horarioConsultas">Horário de Consultas</label>
+                                    <select
+                                        id="horarioConsultas"
+                                        value={horarioConsultas}
+                                        className='select-field w-full'
+                                        onChange={(e) => setHorarioConsultas(e.target.value)}
+                                        required
+                                    >
+                                        <option value="" disabled>Selecione um horário</option>
+                                        {Array.from({ length: 9 }, (_, i) => {
+                                            const hour = (8 + i).toString().padStart(2, '0');
+                                            if (hour === "12") return null;
+                                            return (
+                                                <option key={hour} value={`${hour}:00`}>
+                                                    {`${hour}:00`}
+                                                </option>
+                                            );
+                                        })}
+                                    </select>
+                                </div>
+
                             </div>
 
                             <CheckBox
