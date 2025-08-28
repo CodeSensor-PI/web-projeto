c50b78a8384bbebf08a3a7721a483008b523bf52
GuiGavassa <guilhermegavassa.tecn@gmail.com>
2025-08-28 20:16:32 -0300

    feat: tela de desconectar OK, ajustes landing e login

---
+++ src/pages/components/Desconectado.jsx
@@ -0,0 +1,22 @@
+import './desconectado.css'
+import LogoBlue from '../../assets/images/logoBlue.svg'
+import { useEffect } from 'react'
+
+const Desconectado = () => {
+
+    useEffect(() => {
+        localStorage.clear();
+    }, []);
+
+    return (
+        <div className='desconectado'>
+            <img src={LogoBlue} alt="Logo" />
+            <div className='mensagem'>
+                Você foi desconectado(a).
+                <a href="/login">Clique aqui para fazer login novamente.</a>
+            </div>
+        </div>
+    )
+}
+
+export default Desconectado
