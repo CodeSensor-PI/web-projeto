4bf1f219533aa53ab874e8b26bde6e830edef9f0
KauanPaschoal <kauan.psantos@sptech.school>
2025-11-02 22:42:02 -0300

    refactor: update baseURL initialization to prioritize window.env variables in API providers

--- src/provider/api/api-relatorios.js
+++ src/provider/api/api-relatorios.js
@@ -1,7 +1,10 @@
 import axios from "axios";
 
 const apiRelatorios = axios.create({
-  baseURL: import.meta.env.VITE_RELATORIOS_URL ?? "http://localhost:8081",
+  baseURL:
+    window?.env?.VITE_RELATORIOS_URL ??
+    import.meta.env.VITE_RELATORIOS_URL ??
+    "http://localhost:8081",
   withCredentials: true,
 });
 
